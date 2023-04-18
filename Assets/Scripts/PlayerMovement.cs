8238e7624fc6160d2f86f26d7844f60c99a5be6c
Brolocuelo <luisalfonsorello@gmail.com>
2023-04-18 10:04:40 +0200

    Siguiendo

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,42 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private const string Horizontal ="Horizontal";
-
     public float speed = 15f;
+
+    private Rigidbody playerRb;
+    public float jumpForce;
+
+    private bool isOnTheGround = true;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
 
     private void Update()
     {
         float horizontalInput = Input.GetAxis(Horizontal);
-
         transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
+
+        if (Input.GetKeyDown(KeyCode.Space)&& isOnTheGround)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isOnTheGround = false;
+    }
+
+    private void OnCollisionEnter(Collision otherCollider)
+    {
+        isOnTheGround = true;
+        if (otherCollider.gameObject.CompareTag("Ground"))
+        {
+            isOnTheGround = true;
+        }
     }
 }
